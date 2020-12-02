435c74805a6bae8dad8d2d31fedc932725e21740
MikeJongenVention <mike.jongen@vention.nl>
2020-12-02 09:21:59 +0100

    Initial commit

---
+++ spec/day-2-password-philophy_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Day::2::Password::Philophy do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/day-2-password-philophy.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Day::2::Password::Philophy`
+module Day::2::Password::Philophy
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
