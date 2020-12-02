823f2c083a76bb2967bcca1d887c191f0e805563
MikeJongenVention <mike.jongen@vention.nl>
2020-12-02 09:32:39 +0100

    Argument handling + benchmark + file read

--- src/day-2-password-philophy.cr
+++ src/day-2-password-philophy.cr
@@ -1,6 +1,38 @@
-# TODO: Write documentation for `Day::2::Password::Philophy`
-module Day::2::Password::Philophy
-  VERSION = "0.1.0"
+require "option_parser"
+require "benchmark"
 
-  # TODO: Put your code here
+file_name = ""
+benchmark = false
+
+OptionParser.parse do |parser|
+  parser.banner = "Welcome to Report Repair"
+
+  parser.on "-f FILE", "--file=FILE", "Input file" do |file|
+    file_name = file
+  end
+  parser.on "-b", "--benchmark", "Measure benchmarks" do
+    benchmark = true
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+end
+
+unless file_name.empty?
+  passwords = File.read_lines(file_name)
+  
+  if benchmark
+    Benchmark.ips do |x|
+      x.report("passwords") {result = number_of_valid_passwords(passwords)}
+    end
+  else
+    result = number_of_valid_passwords(passwords)
+  end
+
+  puts result
+end
+
+def number_of_valid_passwords(passwords : Array(String))
+  return 0
 end
